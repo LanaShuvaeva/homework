a45a9e1d1ceb01fba2e1b24344a00fe0d8fe1e1a
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2018-02-23 11:03:53 -0800

    Refactor solution for log1to100

--- log1to100/solution.js
+++ log1to100/solution.js
@@ -1,3 +1,12 @@
+// Homework: log 1 to 100
+// This logs to the screen all
+// numbers from 1 to 100
+
+function log1to100(){}
 for (let i = 1; i < 100; i++) {
   console.log(i);
 }
+}
+log1to100();
+
+//😺
